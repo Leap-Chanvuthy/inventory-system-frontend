a2692d23581ed83abc068564c2e3efc9a77fc61b
Elon Thy <115206732+Leap-Chanvuthy@users.noreply.github.com>
2024-08-20 14:17:21 +0700

    add : authentication slice

---
+++ src/redux/slices/authSlice.js
@@ -0,0 +1,41 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  currentUser: null,
+  error: null,
+  loading: false,  // Ensure loading is initially set to false
+};
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {
+    signInStart(state) {
+      state.loading = true;
+      state.error = null;
+    },
+    signInSuccess(state, action) {
+      state.loading = false;
+      state.currentUser = action.payload;
+      state.error = null;
+    },
+    signInFailure(state, action) {
+      state.loading = false;
+      state.error = action.payload;
+    },
+    logout(state) {
+      state.currentUser = null;
+      state.error = null;
+      state.loading = false;
+    },
+  },
+});
+
+export default authSlice.reducer;
+export const {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+  logout,
+  updateUserProfile,
+} = authSlice.actions;
