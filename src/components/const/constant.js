8a28d66c00fb6a6e07b8a9d3a4100c07a426b3f6
Elon Thy <115206732+Leap-Chanvuthy@users.noreply.github.com>
2024-08-25 19:09:59 +0700

    test ngrok

--- src/components/const/constant.js
+++ src/components/const/constant.js
@@ -1,2 +1,5 @@
-export const BASE_URL = 'http://127.0.0.1:8000/api';
-export const BASE_IMAGE_URL = 'http://127.0.0.1:8000/storage';
+// export const BASE_URL = 'http://127.0.0.1:8000/api';
+// export const BASE_IMAGE_URL = 'http://127.0.0.1:8000/storage';
+
+export const BASE_URL = 'https://a624-203-144-88-57.ngrok-free.app/api';
+export const BASE_IMAGE_URL = 'https://a624-203-144-88-57.ngrok-free.app/storage';
