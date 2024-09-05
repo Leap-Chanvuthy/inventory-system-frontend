bb97b00e6ae9f9d044a9b70f1683f1c8451eb9f0
Elon Thy <115206732+Leap-Chanvuthy@users.noreply.github.com>
2024-09-05 16:55:19 +0700

    create supplier form

--- src/components/ToastNotification.jsx
+++ src/components/ToastNotification.jsx
@@ -1,49 +1,7 @@
-// import { Toast } from "flowbite-react";
-// import { HiCheck, HiExclamation, HiX } from "react-icons/hi";
-
-// export const SuccessToast = ({message}) => {
-//   return (
-//     <Toast>
-//       <div className="inline-flex h-8 w-8 shrink-0 items-center justify-center rounded-lg bg-green-100 text-green-500 dark:bg-green-800 dark:text-green-200">
-//         <HiCheck className="h-5 w-5 bg-tailgreen text-white p-2 rounded-md" />
-//       </div>
-//       <div className="ml-3 text-sm font-normal">Item moved successfully.</div>
-//       <Toast.Toggle />
-//     </Toast>
-//   );
-// };
-
-// export const SangerToast = ({message}) => {
-//   return (
-//     <Toast>
-//       <div className="inline-flex h-8 w-8 shrink-0 items-center justify-center rounded-lg bg-red-100 text-red-500 dark:bg-red-800 dark:text-red-200">
-//         <HiX className="h-5 w-5" />
-//       </div>
-//       <div className="ml-3 text-sm font-normal">Item has been deleted.</div>
-//       <Toast.Toggle />
-//     </Toast>
-//   );
-// };
-
-// export const WarningToast = ({message}) => {
-//   return (
-//     <Toast>
-//       <div className="inline-flex h-8 w-8 shrink-0 items-center justify-center rounded-lg bg-orange-100 text-orange-500 dark:bg-orange-700 dark:text-orange-200">
-//         <HiExclamation className="h-5 w-5" />
-//       </div>
-//       <div className="ml-3 text-sm font-normal">
-//         Improve password difficulty.
-//       </div>
-//       <Toast.Toggle />
-//     </Toast>
-//   );
-// };
-
-
-
 import React from 'react';
 import { Snackbar, Alert, AlertTitle } from '@mui/material';
-import { HiCheck, HiExclamation, HiX } from 'react-icons/hi';
+import { HiCheck, HiExclamation } from 'react-icons/hi';
+import { IoWarningOutline } from "react-icons/io5";
 
 export const SuccessToast = ({ open, onClose, message }) => {
   return (
@@ -79,7 +37,7 @@ export const DangerToast = ({ open, onClose, message }) => {
       <Alert
         onClose={onClose}
         severity="error"
-        icon={<HiX className="text-red-500" />}
+        icon={<IoWarningOutline className="text-red-500" />}
         sx={{
           backgroundColor: 'bg-red-100 dark:bg-red-800',
           color: 'red-500',
@@ -87,7 +45,7 @@ export const DangerToast = ({ open, onClose, message }) => {
           alignItems: 'center',
         }}
       >
-        <AlertTitle>Error</AlertTitle>
+        {/* <AlertTitle>Error</AlertTitle> */}
         {message || 'An error occurred.'}
       </Alert>
     </Snackbar>
--- src/pages/inventory/supplier/partials/CreateForm.jsx
+++ src/pages/inventory/supplier/partials/CreateForm.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
-import { Button, Label, TextInput } from "flowbite-react";
+import { Button, Label, TextInput, FileInput, Textarea } from "flowbite-react";
 
 const mapContainerStyle = {
   width: "100%",
@@ -18,31 +18,65 @@ const center = {
 };
 
 const CreateForm = () => {
-  
-  const [values , setValues] = useState({
-    latitute : "",
-    longitude : ""
+  const [values, setValues] = useState({
+    image: "",
+    name: "",
+    phone_number: "",
+    location: "",
+    longitude: "",
+    latitude: "",
+    address: "",
+    city: "",
+    email: "",
+    contact_person: "",
+    business_registration_number: "",
+    vat_number: "",
+    bank_account_number: "",
+    bank_account_name: "",
+    bank_name: "",
+    note: "",
   });
 
   console.log(values);
 
+  const [selectedLocation, setSelectedLocation] = useState(null);
+  const [imagePreview, setImagePreview] = useState(null);
+
   const { isLoaded, loadError } = useLoadScript({
     googleMapsApiKey: "AIzaSyAd4rEAQqf58fCJGABqW99teDP9BcuyN08",
   });
-
-  const [selectedLocation, setSelectedLocation] = useState(null);
 
   const handleMapClick = (event) => {
     const lat = event.latLng.lat();
     const lng = event.latLng.lng();
     setSelectedLocation({ lat, lng });
 
-        setValues({
-          ...values,       
-          latitute: lat,   
-          longitude: lng, 
-        });
-      
+    setValues((prevValues) => ({
+      ...prevValues,
+      latitude: lat,
+      longitude: lng,
+    }));
+  };
+
+  const handleChange = (e) => {
+    const { id, value } = e.target;
+    setValues((prevValues) => ({
+      ...prevValues,
+      [id]: value,
+    }));
+  };
+
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    if (file && file.type.startsWith("image/")) {
+      setValues((prevValues) => ({
+        ...prevValues,
+        image: file,
+      }));
+      setImagePreview(URL.createObjectURL(file));
+    } else {
+      alert("Please upload a valid image file.");
+    }
   };
 
   if (loadError) return <div>Error loading maps</div>;
@@ -52,8 +86,43 @@ const CreateForm = () => {
     <div>
       <h1>Create Supplier Infos</h1>
       <form className="w-full flex flex-col gap-4 my-5">
-        <div className="flex flex-col lg:md:flex-row gap-3">
-          {/* Name */}
+        <div className="relative flex items-center justify-center">
+          <Label
+            htmlFor="image"
+            className={`flex items-center justify-center cursor-pointer rounded-full border-2 border-dashed border-gray-300 bg-gray-50 hover:bg-gray-100 dark:border-gray-600 dark:bg-gray-700 dark:hover:border-gray-500 dark:hover:bg-gray-600 ${
+              imagePreview ? "p-0" : "p-10"
+            }`}
+            style={{ width: "200px", height: "200px" }}
+          >
+            {imagePreview ? (
+              <img
+                src={imagePreview}
+                alt="Preview"
+                className="w-full h-full object-cover rounded-full"
+              />
+            ) : (
+              <>
+                <svg
+                  className="text-gray-500"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                  />
+                </svg>
+              </>
+            )}
+            <FileInput id="image" className="hidden" onChange={handleFileChange} />
+          </Label>
+        </div>
+
+        <div className="flex flex-col lg:flex-row gap-3">
           <div className="w-full">
             <div className="mb-2 block">
               <Label htmlFor="name" value="Supplier Name" />
@@ -62,11 +131,13 @@ const CreateForm = () => {
               id="name"
               type="text"
               placeholder="Supplier Name"
+              value={values.name}
+              onChange={handleChange}
               required
               shadow
             />
           </div>
-          {/* Phone Number */}
+
           <div className="w-full">
             <div className="mb-2 block">
               <Label htmlFor="phone_number" value="Phone Number" />
@@ -75,11 +146,13 @@ const CreateForm = () => {
               id="phone_number"
               type="text"
               placeholder="Phone Number"
+              value={values.phone_number}
+              onChange={handleChange}
               required
               shadow
             />
           </div>
-          {/* Location */}
+
           <div className="w-full">
             <div className="mb-2 block">
               <Label htmlFor="location" value="Location" />
@@ -88,14 +161,15 @@ const CreateForm = () => {
               id="location"
               type="text"
               placeholder="Location"
+              value={values.location}
+              onChange={handleChange}
               required
               shadow
             />
           </div>
         </div>
 
-        <div className="flex flex-col lg:md:flex-row gap-3">
-          {/* Longitude */}
+        <div className="flex flex-col lg:flex-row gap-3">
           <div className="w-full">
             <div className="mb-2 block">
               <Label htmlFor="longitude" value="Longitude" />
@@ -104,10 +178,12 @@ const CreateForm = () => {
               id="longitude"
               type="text"
               placeholder="Longitude"
+              value={values.longitude}
+              onChange={handleChange}
               shadow
             />
           </div>
-          {/* Latitude */}
+
           <div className="w-full">
             <div className="mb-2 block">
               <Label htmlFor="latitude" value="Latitude" />
@@ -116,27 +192,42 @@ const CreateForm = () => {
               id="latitude"
               type="text"
               placeholder="Latitude"
+              value={values.latitude}
+              onChange={handleChange}
               shadow
             />
           </div>
-          {/* Address */}
+
           <div className="w-full">
             <div className="mb-2 block">
               <Label htmlFor="address" value="Address" />
             </div>
-            <TextInput id="address" type="text" placeholder="Address" shadow />
+            <TextInput
+              id="address"
+              type="text"
+              placeholder="Address"
+              value={values.address}
+              onChange={handleChange}
+              shadow
+            />
           </div>
         </div>
 
-        <div className="flex flex-col lg:md:flex-row gap-3">
-          {/* City */}
+        <div className="flex flex-col lg:flex-row gap-3">
           <div className="w-full">
             <div className="mb-2 block">
               <Label htmlFor="city" value="City" />
             </div>
-            <TextInput id="city" type="text" placeholder="City" shadow />
+            <TextInput
+              id="city"
+              type="text"
+              placeholder="City"
+              value={values.city}
+              onChange={handleChange}
+              shadow
+            />
           </div>
-          {/* Email */}
+
           <div className="w-full">
             <div className="mb-2 block">
               <Label htmlFor="email" value="Email" />
@@ -145,11 +236,13 @@ const CreateForm = () => {
               id="email"
               type="email"
               placeholder="Email"
+              value={values.email}
+              onChange={handleChange}
               required
               shadow
             />
           </div>
-          {/* Contact Person */}
+
           <div className="w-full">
             <div className="mb-2 block">
               <Label htmlFor="contact_person" value="Contact Person" />
@@ -158,29 +251,29 @@ const CreateForm = () => {
               id="contact_person"
               type="text"
               placeholder="Contact Person"
+              value={values.contact_person}
+              onChange={handleChange}
               required
               shadow
             />
           </div>
         </div>
 
-        <div className="flex flex-col lg:md:flex-row gap-3">
-          {/* Business Registration Number */}
+        <div className="flex flex-col lg:flex-row gap-3">
           <div className="w-full">
             <div className="mb-2 block">
-              <Label
-                htmlFor="business_registration_number"
-                value="Business Registration Number"
-              />
+              <Label htmlFor="business_registration_number" value="Business Registration Number" />
             </div>
             <TextInput
               id="business_registration_number"
               type="text"
               placeholder="Business Registration Number"
+              value={values.business_registration_number}
+              onChange={handleChange}
               shadow
             />
           </div>
-          {/* VAT Number */}
+
           <div className="w-full">
             <div className="mb-2 block">
               <Label htmlFor="vat_number" value="VAT Number" />
@@ -189,28 +282,28 @@ const CreateForm = () => {
               id="vat_number"
               type="text"
               placeholder="VAT Number"
+              value={values.vat_number}
+              onChange={handleChange}
               shadow
             />
           </div>
-          {/* Bank Account Number */}
+
           <div className="w-full">
             <div className="mb-2 block">
-              <Label
-                htmlFor="bank_account_number"
-                value="Bank Account Number"
-              />
+              <Label htmlFor="bank_account_number" value="Bank Account Number" />
             </div>
             <TextInput
               id="bank_account_number"
               type="text"
               placeholder="Bank Account Number"
+              value={values.bank_account_number}
+              onChange={handleChange}
               shadow
             />
           </div>
         </div>
 
-        <div className="flex flex-col lg:md:flex-row gap-3">
-          {/* Bank Account Name */}
+        <div className="flex flex-col lg:flex-row gap-3">
           <div className="w-full">
             <div className="mb-2 block">
               <Label htmlFor="bank_account_name" value="Bank Account Name" />
@@ -219,10 +312,12 @@ const CreateForm = () => {
               id="bank_account_name"
               type="text"
               placeholder="Bank Account Name"
+              value={values.bank_account_name}
+              onChange={handleChange}
               shadow
             />
           </div>
-          {/* Bank Name */}
+
           <div className="w-full">
             <div className="mb-2 block">
               <Label htmlFor="bank_name" value="Bank Name" />
@@ -231,29 +326,29 @@ const CreateForm = () => {
               id="bank_name"
               type="text"
               placeholder="Bank Name"
-              shadow
-            />
-          </div>
-          {/* Note */}
-          <div className="w-full">
-            <div className="mb-2 block">
-              <Label htmlFor="note" value="Note" />
-            </div>
-            <TextInput
-              id="note"
-              type="text"
-              placeholder="Note"
-              required
+              value={values.bank_name}
+              onChange={handleChange}
               shadow
             />
           </div>
         </div>
 
-        {/* Map Picker */}
-        <div>
+        <div className="w-full">
           <div className="mb-2 block">
-            <Label htmlFor="repeat-password" value="Pick Location" />
+            <Label htmlFor="note" value="Note" />
           </div>
+          <Textarea
+            cols={3}
+            id="note"
+            type="text"
+            placeholder="Note"
+            value={values.note}
+            onChange={handleChange}
+            shadow
+          />
+        </div>
+
+        <div className="w-full mb-4" style={{ height: "400px" }}>
           <GoogleMap
             mapContainerStyle={mapContainerStyle}
             zoom={10}
@@ -261,18 +356,15 @@ const CreateForm = () => {
             options={options}
             onClick={handleMapClick}
           >
-            {selectedLocation && <Marker position={selectedLocation} />}
+            {selectedLocation && (
+              <Marker position={selectedLocation} />
+            )}
           </GoogleMap>
-          {selectedLocation && (
-            <div className="mt-4">
-              <p>Latitude: {selectedLocation.lat}</p>
-              <p>Longitude: {selectedLocation.lng}</p>
-            </div>
-          )}
         </div>
 
-        {/* Submit Button */}
-        <Button type="submit">Submit Supplier Info</Button>
+        <Button type="submit">
+          Save
+        </Button>
       </form>
     </div>
   );
