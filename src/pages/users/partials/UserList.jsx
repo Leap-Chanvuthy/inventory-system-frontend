a0121a24e9f7f7832d22de4150cd3b004f6fbc2b
Elon Thy <115206732+Leap-Chanvuthy@users.noreply.github.com>
2024-09-16 15:58:33 +0700

    modify user listing

--- src/pages/users/partials/UserList.jsx
+++ src/pages/users/partials/UserList.jsx
@@ -1,162 +1,51 @@
-import { useEffect, useState } from "react";
-import { Table, TextInput, Button, Select } from "flowbite-react";
+import { useState } from "react";
+import { TextInput, Button, Select } from "flowbite-react";
 import { IoSearchOutline } from "react-icons/io5";
 import { Link } from "react-router-dom";
-import GlobalPagination from "../../../components/Pagination";
-import { useSelector, useDispatch } from "react-redux";
-import axios from "axios";
-import { BASE_URL, BASE_IMAGE_URL } from "../../../components/const/constant";
-import {
-  fetchUsersStart,
-  fetchUsersSuccess,
-  fetchUsersFailure,
-} from "../../../redux/slices/userSlice";
-import LoadingState from "./LoadingState";
+import UserTable from "./list/UserTable";
 
 function UserList() {
-  const dispatch = useDispatch();
-  const { users, status, error } = useSelector((state) => state.users);
 
-  // handle search query
-  const [values, setValues] = useState({
-    query: "",
-    role: "",
-  });
+  const [filters , setFilters] = useState({
+    search : "",
+    role : ""
+  })
 
-  const handleChangeValue = (e) => {
-    const key = e.target.id;
-    const value = e.target.value;
-    setValues({ ...values, [key]: value });
-  };
+  const handleFilterChange = (e) =>{
+    const {id , value} = e.target;
+    setFilters({...filters , [id] : value});
+  }
 
-  // pagination and refetch data
-  const [currentPage, setCurrentPage] = useState(1);
-
-  useEffect(() => {
-    fetchData(currentPage, values.query, values.role);
-  }, [currentPage, values.query, values.role]);
-
-  const submit = (e) => {
+  const handleSearch = (e) => {
     e.preventDefault();
-    fetchData(currentPage, values.query, values.role);
-  };
+  }
 
-  const fetchData = async (page = 1, query = "", role = "") => {
-    dispatch(fetchUsersStart());
-    try {
-      const response = await axios.get(`${BASE_URL}/users`, {
-        params: {
-          page,
-          "filter[name]": query,
-          "filter[phone_number]": query,
-          "filter[role]": role,
-        },
-      });
-      dispatch(fetchUsersSuccess(response.data));
-      setCurrentPage(response.data.current_page);
-    } catch (error) {
-      console.error("Fetch Users Error:", error);
-      dispatch(fetchUsersFailure("Error fetching users: " + error.message));
-    }
-  };
-
-  const handlePageChange = (event, newPage) => {
-    setCurrentPage(newPage);
-  };
-
-  if (status === "loading") return <LoadingState />;
-  if (status === "failed")
-    return <div className="text-center py-5 text-red-500">{error}</div>;
 
   return (
     <div>
       <div className="my-5 flex flex-col lg:flex-row gap-3 justify-between">
-        <Link to="/users/create">
-          <Button color="info">Create New</Button>
-        </Link>
-        <form onSubmit={submit} className="flex gap-3">
+        <form onSubmit={handleSearch}  className="flex gap-3">
           <TextInput
             placeholder="Search name, phone number"
-            id="query"
-            value={values.query}
-            onChange={handleChangeValue}
+            id="search"
+            value={filters.search}
+            onChange={handleFilterChange}
             rightIcon={IoSearchOutline}
           />
-          <Select id="role" value={values.role} onChange={handleChangeValue}>
+          <Select id="role" value={filters.role} onChange={handleFilterChange}>
             <option value="">Role</option>
             <option value="ADMIN">Admin</option>
             <option value="VENDOR">Stock Controller</option>
             <option value="USER">User</option>
           </Select>
-          <Button type="submit">Search</Button>
         </form>
+        <Link to="/users/create">
+          <Button color="info">Create New</Button>
+        </Link>
       </div>
-      <div className="overflow-x-auto h-[65vh] overflow-y-scroll my-5">
-        <Table striped>
-          <Table.Head>
-            <Table.HeadCell>No.</Table.HeadCell>
-            <Table.HeadCell>Image</Table.HeadCell>
-            <Table.HeadCell>Name</Table.HeadCell>
-            <Table.HeadCell>Email</Table.HeadCell>
-            <Table.HeadCell>Phone Number</Table.HeadCell>
-            <Table.HeadCell>Role</Table.HeadCell>
-            <Table.HeadCell>Email Verified At</Table.HeadCell>
-            <Table.HeadCell>
-              <span className="sr-only">Edit</span>
-            </Table.HeadCell>
-          </Table.Head>
-          <Table.Body className="divide-y">
-            {users.data && users.data.length > 0 ? (
-              users.data.map((user, index) => (
-                <Table.Row
-                  key={user.id}
-                  className="bg-white dark:border-gray-700 dark:bg-gray-800"
-                >
-                  <Table.Cell>{index + 1}</Table.Cell>
-                  <Table.Cell>
-                    <img
-                      src={`${BASE_IMAGE_URL}/${user.profile_picture}`}
-                      alt={user.name}
-                      className="h-10 w-10 rounded-full object-cover"
-                    />
-                  </Table.Cell>
-                  <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-                    {user.name}
-                  </Table.Cell>
-                  <Table.Cell>{user.email}</Table.Cell>
-                  <Table.Cell>{user.phone_number}</Table.Cell>
-                  <Table.Cell>{user.role}</Table.Cell>
-                  <Table.Cell>{user.email_verified_at || "N/A"}</Table.Cell>
-                  <Table.Cell>
-                    <Link
-                      to={`/users/edit/${user.id}`}
-                      className="font-medium text-cyan-600 hover:underline dark:text-cyan-500"
-                    >
-                      Edit
-                    </Link>
-                  </Table.Cell>
-                </Table.Row>
-              ))
-            ) : (
-              <Table.Row>
-                <Table.Cell colSpan="8" className="text-center py-4">
-                  No users found.
-                </Table.Cell>
-              </Table.Row>
-            )}
-          </Table.Body>
-        </Table>
-      </div>
-      {users && (
-        <GlobalPagination
-          current_page={users.current_page}
-          last_page={users.last_page}
-          from={users.from}
-          to={users.to}
-          total={users.total}
-          onPageChange={handlePageChange}
-        />
-      )}
+
+      <UserTable filters={filters} />
+
     </div>
   );
 }
---
+++ src/pages/users/partials/list/UserTable.jsx
@@ -0,0 +1,172 @@
+import { useEffect, useState } from "react";
+import { Table } from "flowbite-react";
+import { Link } from "react-router-dom";
+import GlobalPagination from "../../../../components/Pagination";
+import { useSelector, useDispatch } from "react-redux";
+import axios from "axios";
+import {
+  BASE_URL,
+  BASE_IMAGE_URL,
+} from "../../../../components/const/constant";
+import {
+  fetchUsersStart,
+  fetchUsersSuccess,
+  fetchUsersFailure,
+} from "../../../../redux/slices/userSlice";
+import LoadingState from "../../../inventory/supplier/partials/list/LoadingState";
+
+function UserTable({ filters }) {
+  const dispatch = useDispatch();
+  const { users, status, error } = useSelector((state) => state.users);
+  console.log(users);
+
+  // Fetch data & pagination
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+  const [totalItems, setTotalItems] = useState(0);
+
+  const fetchData = async (page = 1) => {
+    dispatch(fetchUsersStart());
+    try {
+      const response = await axios.get(`${BASE_URL}/users`, {
+        params: {
+          page,
+          "filter[search]" : filters.search,
+          "filter[role]" : filters.role
+        },
+      });
+      dispatch(fetchUsersSuccess(response.data.data));
+      setCurrentPage(response.data.current_page);
+      setTotalPages(response.data.last_page);
+      setTotalItems(response.data.total);
+    } catch (err) {
+      console.error("Failed to fetch user:", err);
+      dispatch(
+        fetchUsersFailure(
+          err.message || "Failed to fetch data from the server."
+        )
+      );
+    }
+  };
+
+  useEffect(() => {
+    fetchData(currentPage);
+  }, [filters, currentPage]);
+
+  const handlePageChange = (event, newPage) => {
+    setCurrentPage(newPage);
+  };
+
+  if (status === "loading")
+    return (
+      <div>
+        <LoadingState />
+        <GlobalPagination
+          current_page={currentPage}
+          last_page={totalPages}
+          from={(currentPage - 1) * 10 + 1}
+          to={Math.min(currentPage * 10, totalItems)}
+          total={totalItems}
+          onPageChange={handlePageChange}
+        />
+      </div>
+    );
+
+  if (!Array.isArray(users)) {
+    return (
+      <div className="text-center py-5 text-red-500">
+        users data is invalid.
+      </div>
+    );
+  }
+
+  if (users.length === 0) {
+    return (
+    <div className="overflow-x-auto h-[65vh] overflow-y-scroll my-5">
+        <Table hoverable>
+          <Table.Head>
+            <Table.HeadCell>No.</Table.HeadCell>
+            <Table.HeadCell>Image</Table.HeadCell>
+            <Table.HeadCell>Name</Table.HeadCell>
+            <Table.HeadCell>Email</Table.HeadCell>
+            <Table.HeadCell>Phone Number</Table.HeadCell>
+            <Table.HeadCell>Role</Table.HeadCell>
+            <Table.HeadCell>Email Verified At</Table.HeadCell>
+          </Table.Head>
+          <Table.Body className="devide-y">
+            <Table.Row>
+                <Table.Cell colSpan="8" className="text-center py-4">
+                  No users found. :)
+                </Table.Cell>
+              </Table.Row>
+          </Table.Body>
+        </Table>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <div className="overflow-x-auto h-[65vh] overflow-y-scroll my-5">
+        <Table hoverable>
+          <Table.Head>
+            <Table.HeadCell>No.</Table.HeadCell>
+            <Table.HeadCell>Image</Table.HeadCell>
+            <Table.HeadCell>Name</Table.HeadCell>
+            <Table.HeadCell>Email</Table.HeadCell>
+            <Table.HeadCell>Phone Number</Table.HeadCell>
+            <Table.HeadCell>Role</Table.HeadCell>
+            <Table.HeadCell>Email Verified At</Table.HeadCell>
+            <Table.HeadCell>
+              <span className="sr-only">Edit</span>
+            </Table.HeadCell>
+          </Table.Head>
+          <Table.Body className="divide-y">
+            {  users.map((user, index) => (
+                <Table.Row
+                  key={user.id}
+                  className="bg-white dark:border-gray-700 dark:bg-gray-800"
+                >
+                  <Table.Cell>{index + 1}</Table.Cell>
+                  <Table.Cell>
+                    <img
+                      src={`${BASE_IMAGE_URL}/${user.profile_picture}`}
+                      alt={user.name}
+                      className="h-10 w-10 rounded-full object-cover"
+                    />
+                  </Table.Cell>
+                  <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                    {user.name}
+                  </Table.Cell>
+                  <Table.Cell>{user.email}</Table.Cell>
+                  <Table.Cell>{user.phone_number}</Table.Cell>
+                  <Table.Cell>{user.role}</Table.Cell>
+                  <Table.Cell>{user.email_verified_at || "N/A"}</Table.Cell>
+                  <Table.Cell>
+                    <Link
+                      to={`/users/edit/${user.id}`}
+                      className="font-medium text-cyan-600 hover:underline dark:text-cyan-500"
+                    >
+                      Edit
+                    </Link>
+                  </Table.Cell>
+                </Table.Row>
+              ))}
+          </Table.Body>
+        </Table>
+      </div>
+      <div className="my-5">
+        <GlobalPagination
+          current_page={currentPage}
+          last_page={totalPages}
+          from={(currentPage - 1) * 10 + 1}
+          to={Math.min(currentPage * 10, totalItems)}
+          total={totalItems}
+          onPageChange={handlePageChange}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default UserTable;
