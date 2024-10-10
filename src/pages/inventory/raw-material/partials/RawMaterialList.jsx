7435df1fbbf6924444f18413598bd955bf6a40c0
Elon Thy <115206732+Leap-Chanvuthy@users.noreply.github.com>
2024-10-10 17:46:08 +0700

    list raw material and search query

--- src/pages/inventory/raw-material/partials/RawMaterialList.jsx
+++ src/pages/inventory/raw-material/partials/RawMaterialList.jsx
@@ -1,6 +1,6 @@
 // RawMaterialList.js
 import { useState } from "react";
-import { TextInput, Button, Select } from "flowbite-react";
+import { TextInput, Button, Select, Dropdown, Checkbox, Label } from "flowbite-react";
 import { IoSearchOutline } from "react-icons/io5";
 import { Link } from "react-router-dom";
 import ExportRawMaterial from "./ExportRawMaterial";
@@ -11,12 +11,34 @@ const RawMaterialList = () => {
   const [filters, setFilters] = useState({
     query: "",
     category: "",
-    stockLevel: "",
+    status: "",
+    sort: [],
   });
 
   const handleFilterChange = (e) => {
     const { id, value } = e.target;
     setFilters({ ...filters, [id]: value });
+  };
+
+  const handleSortChange = (e) => {
+    const { id, checked } = e.target;
+    if (checked) {
+      setFilters({ ...filters, sort: [...filters.sort, id] });
+    } else {
+      setFilters({
+        ...filters,
+        sort: filters.sort.filter((sortField) => sortField !== id),
+      });
+    }
+  };
+
+  const resetFilters = () => {
+    setFilters({
+      search: "",
+      status: "",
+      category: "",
+      sort: [],
+    });
   };
 
   const handleSearch = (e) => {
@@ -27,7 +49,10 @@ const RawMaterialList = () => {
     <div>
       <div>
         <div className="my-5 flex flex-col lg:md:flex-row gap-3 justify-between">
-          <form onSubmit={handleSearch} className="flex flex-col lg:md:flex-row gap-3">
+          <form
+            onSubmit={handleSearch}
+            className="flex flex-col lg:md:flex-row gap-3"
+          >
             <TextInput
               placeholder="Search product"
               id="query"
@@ -35,19 +60,70 @@ const RawMaterialList = () => {
               onChange={handleFilterChange}
               rightIcon={IoSearchOutline}
             />
-            <Select id="category" value={filters.category} onChange={handleFilterChange}>
-              <option value="">Category</option>
-              <option value="Raw Materials">Raw Materials</option>
-              <option value="Work In Progress">Work In Progress</option>
-              <option value="Packaging">Packaging</option>
-              <option value="Finished Goods">Finished Goods</option>
-            </Select>
-            <Select id="stockLevel" value={filters.stockLevel} onChange={handleFilterChange}>
-              <option value="">Stock Level</option>
-              <option value="Lowest">Lowest</option>
-              <option value="Highest">Highest</option>
-            </Select>
-            <Button type="submit" color="info">Search</Button>
+            <Dropdown label="Sort">
+              <div className="p-2">
+                <div className="flex items-center gap-2">
+                  <Checkbox
+                    id="created_at"
+                    onChange={handleSortChange}
+                    checked={filters.sort.includes("created_at")}
+                  />
+                  <label htmlFor="created_at">Created At</label>
+                </div>
+                <div className="flex items-center gap-2">
+                  <Checkbox
+                    id="updated_at"
+                    onChange={handleSortChange}
+                    checked={filters.sort.includes("updated_at")}
+                  />
+                  <label htmlFor="updated_at">Updated At</label>
+                </div>
+              </div>
+            </Dropdown>
+
+
+                          {/* Filter        */}
+                          <Dropdown label="Filter">
+                <p className="absolute left-[70%] text-red-500 cursor-pointer" onClick={resetFilters}>Clear</p>
+                <div className="p-5">
+                  <div className="flex flex-col gap-3">
+                    <div>
+                      <Label
+                        htmlFor="status"
+                        value="Status"
+                        className="mb-2 block"
+                      />
+                      <Select
+                        id="status"
+                        value={filters.status}
+                        onChange={handleFilterChange}
+                      >
+                        <option value="">Select Status</option>
+                        <option value="IN_STOCK">In stock</option>
+                        <option value="OUT_OF_STOCK">Out of stock</option>
+                      </Select>
+                    </div>
+
+                    <div>
+                      <Label
+                        htmlFor="category"
+                        value="Category"
+                        className="mb-2 block"
+                      />
+                      <Select
+                        id="category"
+                        value={filters.category}
+                        onChange={handleFilterChange}
+                      >
+                        <option value="">Select Category</option>
+                        <option value="SERVICE">Service</option>
+                        <option value="PRODUCT">Product</option>
+                      </Select>
+                    </div>
+                  </div>
+                </div>
+              </Dropdown>
+
           </form>
           <Link to="/raw-materials/create">
             <Button color="info">Create New</Button>
@@ -55,7 +131,6 @@ const RawMaterialList = () => {
         </div>
 
         <div className="flex gap-3 items-center">
-          <ExportRawMaterial />
           <ExportRawMaterial />
         </div>
       </div>
--- src/pages/inventory/raw-material/partials/list/RawMaterialTable.jsx
+++ src/pages/inventory/raw-material/partials/list/RawMaterialTable.jsx
@@ -1,10 +1,139 @@
+// import { useEffect, useState } from "react";
+// import { Table } from "flowbite-react";
+// import { Link } from "react-router-dom";
+// import axios from "axios";
+// import GlobalPagination from "../../../../../components/Pagination";
+// import { BASE_URL } from "../../../../../components/const/constant";
+// import LoadingState from "./LoadingState";
+
+// const RawMaterialTable = ({ filters }) => {
+//   const [rawMaterials, setRawMaterials] = useState([]);
+//   const [status, setStatus] = useState("idle");
+//   const [error, setError] = useState(null);
+//   const [currentPage, setCurrentPage] = useState(1);
+//   const [totalPages, setTotalPages] = useState(1);
+//   const [totalItems, setTotalItems] = useState(0); 
+
+//   const fetchRawMaterials = async (page = 1) => {
+//     setStatus("loading");
+//     try {
+//       const response = await axios.get(`${BASE_URL}/raw-materials`, {
+//         params: {
+//           page,
+//           "filter[search]": filters.query,
+//         },
+//       });
+//       setRawMaterials(response.data.data);
+//       setCurrentPage(response.data.current_page);
+//       setTotalPages(response.data.last_page);
+//       setTotalItems(response.data.total);
+//       setStatus("succeeded");
+//     } catch (err) {
+//       setError("Failed to fetch raw materials: " + err.message);
+//       setStatus("failed");
+//     }
+//   };
+
+//   // Fetch data when filters or page changes
+//   useEffect(() => {
+//     fetchRawMaterials(currentPage);
+//   }, [filters, currentPage]);
+
+//   const handlePageChange = (event, newPage) => {
+//     setCurrentPage(newPage);
+//   };
+
+//   if (status === "loading") return (
+//     <div>
+//       <LoadingState />
+//       <GlobalPagination
+//           current_page={currentPage}
+//           last_page={totalPages}
+//           from={(currentPage - 1) * 10 + 1} // Assuming 10 items per page
+//           to={Math.min(currentPage * 10, totalItems)}
+//           total={totalItems}
+//           onPageChange={handlePageChange}
+//        />
+//     </div>
+//   );
+//   if (status === "failed") return <div className="text-center py-5 text-red-500">{error}</div>;
+
+//   return (
+//     <div>
+//       <div className="overflow-x-auto h-[65vh] overflow-y-scroll my-5">
+//         <Table striped>
+//           <Table.Head>
+//             <Table.HeadCell>Product Name</Table.HeadCell>
+//             <Table.HeadCell>Quantity</Table.HeadCell>
+//             <Table.HeadCell>Unit Price</Table.HeadCell>
+//             <Table.HeadCell>Total Value</Table.HeadCell>
+//             <Table.HeadCell>Supplier</Table.HeadCell>
+//             <Table.HeadCell>Supplier</Table.HeadCell>
+//           </Table.Head>
+//           <Table.Body className="divide-y">
+//             {rawMaterials.length > 0 ? (
+//               rawMaterials.map((material) => (
+//                 <Table.Row
+//                   key={material.id}
+//                   className="bg-white dark:border-gray-700 dark:bg-gray-800"
+//                 >
+//                   <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+//                     {material.name}
+//                   </Table.Cell>
+//                   <Table.Cell>
+//                     {material.quantity} {material.unit}
+//                   </Table.Cell>
+//                   <Table.Cell>{material.unit_price}</Table.Cell>
+//                   <Table.Cell>{material.total_value}</Table.Cell>
+//                   <Table.Cell>{material.supplier?.name || "N/A"}</Table.Cell>
+//                   <Table.Cell>
+//                     <Link
+//                       to={`/raw-materials/edit/${material.id}`}
+//                       className="font-medium text-cyan-600 hover:underline dark:text-cyan-500"
+//                     >
+//                       Edit
+//                     </Link>
+//                   </Table.Cell>
+//                 </Table.Row>
+//               ))
+//             ) : (
+//               <Table.Row>
+//                 <Table.Cell colSpan="6" className="text-center py-4">
+//                   No raw materials found.
+//                 </Table.Cell>
+//               </Table.Row>
+//             )}
+//           </Table.Body>
+//         </Table>
+//       </div>
+
+//       {/* Pagination Controls */}
+//       <div className="my-5">
+//         <GlobalPagination
+//           current_page={currentPage}
+//           last_page={totalPages}
+//           from={(currentPage - 1) * 10 + 1} // Assuming 10 items per page
+//           to={Math.min(currentPage * 10, totalItems)}
+//           total={totalItems}
+//           onPageChange={handlePageChange}
+//         />
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default RawMaterialTable;
+
+
+
 import { useEffect, useState } from "react";
-import { Table } from "flowbite-react";
+import { Avatar, Badge, Table } from "flowbite-react";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import GlobalPagination from "../../../../../components/Pagination";
-import { BASE_URL } from "../../../../../components/const/constant";
+import { BASE_URL , BASE_IMAGE_URL } from "../../../../../components/const/constant";
 import LoadingState from "./LoadingState";
+import formatDistanceToNow from 'date-fns/formatDistanceToNow';
 
 const RawMaterialTable = ({ filters }) => {
   const [rawMaterials, setRawMaterials] = useState([]);
@@ -21,6 +150,9 @@ const RawMaterialTable = ({ filters }) => {
         params: {
           page,
           "filter[search]": filters.query,
+          "filter[status]" : filters.status,
+          "filter[raw_material_category]" : filters.category ,
+          "sort" : filters.sort
         },
       });
       setRawMaterials(response.data.data);
@@ -60,15 +192,21 @@ const RawMaterialTable = ({ filters }) => {
 
   return (
     <div>
-      <div className="overflow-x-auto h-[65vh] overflow-y-scroll my-5">
+      <div className="overflow-x-auto lg:max-w-6xl  my-5">
         <Table striped>
           <Table.Head>
+            <Table.HeadCell>Image</Table.HeadCell>
+            <Table.HeadCell>Code</Table.HeadCell>
             <Table.HeadCell>Product Name</Table.HeadCell>
+            <Table.HeadCell>Status</Table.HeadCell>
             <Table.HeadCell>Quantity</Table.HeadCell>
             <Table.HeadCell>Unit Price</Table.HeadCell>
             <Table.HeadCell>Total Value</Table.HeadCell>
-            <Table.HeadCell>Supplier</Table.HeadCell>
-            <Table.HeadCell>Supplier</Table.HeadCell>
+            <Table.HeadCell>Minimum Stock</Table.HeadCell>
+            <Table.HeadCell>location</Table.HeadCell>
+            <Table.HeadCell>Created</Table.HeadCell>
+            <Table.HeadCell>Updated</Table.HeadCell>
+            <Table.HeadCell>Actions</Table.HeadCell>
           </Table.Head>
           <Table.Body className="divide-y">
             {rawMaterials.length > 0 ? (
@@ -77,15 +215,39 @@ const RawMaterialTable = ({ filters }) => {
                   key={material.id}
                   className="bg-white dark:border-gray-700 dark:bg-gray-800"
                 >
+                  <Table.Cell className="whitespace-nowrap">
+                    {material.raw_material_images.length > 0 ? (
+                      <Avatar
+                        img={`${BASE_IMAGE_URL}/${material.raw_material_images[0].image}`}
+                        alt={material.name}
+                        className="w-16 h-16 object-cover"
+                      />
+                    ) : (
+                      <span>No Image</span>
+                    )}
+                  </Table.Cell>
+                  <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                    {material.material_code}
+                  </Table.Cell>
                   <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
                     {material.name}
+                  </Table.Cell>
+                  <Table.Cell>
+                    {material.status === 'IN_STOCK' && (
+                      <Badge color="success">{material.status}</Badge>
+                    )}
+                    {material.status === 'OUT_OF_STOCK' && (
+                      <Badge color="warning">{material.status}</Badge>
+                    )}
                   </Table.Cell>
                   <Table.Cell>
                     {material.quantity} {material.unit}
                   </Table.Cell>
                   <Table.Cell>{material.unit_price}</Table.Cell>
                   <Table.Cell>{material.total_value}</Table.Cell>
-                  <Table.Cell>{material.supplier?.name || "N/A"}</Table.Cell>
+                  <Table.Cell>{material.minimum_stock_level}</Table.Cell>
+                  <Table.Cell>{material.location}</Table.Cell>
+                  <Table.Cell>{formatDistanceToNow(new Date(material.created_at))}</Table.Cell>
                   <Table.Cell>
                     <Link
                       to={`/raw-materials/edit/${material.id}`}
@@ -98,7 +260,7 @@ const RawMaterialTable = ({ filters }) => {
               ))
             ) : (
               <Table.Row>
-                <Table.Cell colSpan="6" className="text-center py-4">
+                <Table.Cell colSpan="8" className="text-center py-4">
                   No raw materials found.
                 </Table.Cell>
               </Table.Row>
