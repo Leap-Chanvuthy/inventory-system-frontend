798c8768316777aefc1339d5ae9917fd5f011334
Elon Thy <115206732+Leap-Chanvuthy@users.noreply.github.com>
2025-01-10 15:14:39 +0700

    minor change on ui

--- src/pages/inventory/raw-material/RawMaterial.jsx
+++ src/pages/inventory/raw-material/RawMaterial.jsx
@@ -6,6 +6,7 @@ import RawMaterialList from "./partials/RawMaterialList";
 import { IoMdSettings } from "react-icons/io";
 import Create from "./partials/raw-material-scrap/Create";
 import RawMaterialScrapList from "./partials/RawMaterialScrapList";
+import { IoCartOutline } from "react-icons/io5";
 
 const RawMaterial = () => {
   return (
@@ -13,7 +14,7 @@ const RawMaterial = () => {
       <Partial title="raw materials" link="list" />
       <div className="flex flex-col gap-5 my-5">
         <Tabs  aria-label="Tabs with icons"  className="my-5" variant="underline" >
-          <Tabs.Item active title="Raw Materials" icon={IoMdSettings}>
+          <Tabs.Item active title="Raw Materials" icon={IoCartOutline}>
             <RawMaterialList />
             <div className="flex flex-col lg:md:flex-row gap-5">
               <PieChart data={data} title="Category" />
